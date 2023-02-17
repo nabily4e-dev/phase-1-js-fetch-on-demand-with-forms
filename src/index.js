acc7c819c303c70959c986e1f9cfa5a3b71e18ea
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-17 22:30:33 +0300

    First part completed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const init = () => {
     const inputForm = document.querySelector("form");
     inputForm.addEventListener("submit", (event) => {
         event.preventDefault();
-        console.log(event.target.children[1].value);
+        // console.log(event.target.children[1].value);
+
+        const input = document.querySelector('input#searchByID');
+
+        console.log(input.value);
     });
 };
 
